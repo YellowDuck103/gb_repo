b5fbba38ac4a6da3ba200578b10e501e8a378da0
YellowDuck03 <ultrix47@gmail.com>
2021-10-30 21:04:25 +0300

    add homework_2

---
+++ homework_2_scrypt_1.py
@@ -0,0 +1,7 @@
+my_list = [1, 2, True, None, "truba", "fara"]
+#for el in reversed(my_list):
+#	print(type(el))
+my_str = my_list
+for el in reversed(my_str):
+	print(el)
+	print(type(el))
---
+++ homework_2_scrypt_5.py
@@ -0,0 +1,7 @@
+my_list = [7, 5, 3, 3,8]
+for el in my_list:
+    nn = int(input("Введите целое число:  "))
+    my_list.append(nn)
+    my_list.sort()
+    print(my_list)
+    continue
